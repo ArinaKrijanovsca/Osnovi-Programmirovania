07f63655d5635be3711d27e57afa9f00a0d6b48f
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2023-12-15 21:58:59 +0200

    Add files via upload

---
+++ тема 7 практикум 1/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_7_практикум_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for(int i=0;i<=256;i++)
+
+            // {
+
+            // Console.WriteLine($"Код {i} соотв. символу {(char)i}");
+
+            // }
+
+            //Задача 2
+
+            //Console.Title = "Дан символ. Вывести его код";
+
+            //Console.Write("Введите символ: ");
+
+            //char ch = char.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Символ {ch} имеет код {(int)ch} ");
+
+            //Задача 3
+
+            //Console.Title = "Вывести символ, который в кодовой таблице ASCII следует за этим символом";
+
+            //Console.Write("редактируйте символ ");
+
+            //char ch = char.Parse(Console.ReadLine());
+
+            //int код = (int)ch;
+
+            //Console.WriteLine($"За символом {ch} следует {(char)(code+1)}");
+
+            //Задача 6
+
+            //Console.Title = "Дано название футбольного клуба. Распечатать его на экране «столбиком»";
+
+            //Console.Write("Введите название клуба ");
+
+            //строка ch = Console.ReadLine();
+
+            //char[] omas = ch.ToCharArray();
+
+            //for(int i=0;i<omas.Length;i++)
+
+            //{
+
+            // Console.WriteLine($"{omas[i]}");
+
+            //}
+
+            //Задача 7
+
+            //Console.Title = "Составить программу, которая печатает заданное слово, начиная с последних цифр";
+
+            //Console.Write("Введите название клуба ");
+
+            //строка ch = Console.ReadLine();
+
+            //char[] omas = ch.ToCharArray();
+
+            //for (int i = omas.Length -1; i >= 0; i--)
+
+            //{
+
+            // Console.WriteLine($"{omas[i]}");
+
+            //}
+
+            //Задача 8
+
+            //Console.Title = "Преобразовать символы в строку из строк в прописные и обратно";
+
+            //Console.Write("Введите формулировку:");
+
+            //строка ch = Console.ReadLine();
+
+            //char[] s = ch.ToCharArray();
+
+            //for (int i = 0; i < s.Length; i++)
+
+            //{
+
+            // Console.Write(char.ToUpper(s[i]));
+
+            //}
+
+            //Консоль.WriteLine();
+
+            //for (int i = 0; i < s.Length; i++)
+
+            //{
+
+            // Console.Write(char.ToLower(s[i]));
+
+            //}
+
+            //Задача 4
+
+            //Console.Title = "Вывести символ, который в кодовой таблице ASCII следует за этим символом";
+
+            //Console.Write("редактируйте символ ");
+
+            //char ch = char.Parse(Console.ReadLine());
+
+            //int код = (int)ch;
+
+        }
+    }
+}
