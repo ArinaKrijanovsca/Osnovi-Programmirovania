ebde4f5c481aa2db69c626250b64481aa6cbb942
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2023-12-15 21:55:45 +0200

    Add files via upload

--- тема 6 прак 1/Program.cs
+++ тема 6 прак 1/Program.cs
@@ -10,6 +10,155 @@ namespace тема_6_прак_1
     {
         static void Main(string[] args)
         {
+            //Т6кн1
+
+            //1
+
+            //int я = 1;
+
+            //Console.Write("Введите число конца интервала:");
+
+            //int n=int.Parse(Console.ReadLine());
+
+            //for(int i=1; i<=n; i++)
+
+            //{
+
+            // Console.WriteLine(i);
+
+            //}
+
+
+
+            // пока (n >= i)
+
+            //{
+
+            // Console.WriteLine(i);
+
+            // я++;
+
+            //}
+
+
+
+            //делать
+
+            //{
+
+            // Console.WriteLine(i);
+
+            // я++;
+
+            //}
+
+            // пока (n>=i);
+
+
+
+            //2
+
+            //интервал с = 1;
+
+            //for (int i = 10;i <=99;i+=2)
+
+            //{
+
+            //Console.WriteLine(i);
+
+            // с++;
+
+            //}
+
+            //Console.WriteLine(s);
+
+
+
+
+
+            //3
+
+            //двойной час = -5;
+
+            //for (h = -5; h <= 5; h += 0,2)
+
+            //{
+
+            // Console.WriteLine(2 * Math.Pow(h, 2) + 4 * h - 5);
+
+            //}
+
+            // пока (ч <= 5)
+
+            //{
+
+
+
+            // Console.WriteLine(2 * Math.Pow(h, 2) + 4 * h - 5);
+
+            // ч += 0,2;
+
+            //}
+
+            //делать
+
+            //{
+
+            // Console.WriteLine(2 * Math.Pow(h, 2) + 4 * h - 5);
+
+            // ч += 0,2;
+
+            //}
+
+            // пока (ч <= 5);
+
+
+            //4
+            //целая сумма = 0;
+
+            //для (int i=0;i<=1000;i++)
+
+            //{
+
+            // если(я%5==0)
+
+            // {
+
+
+
+            // сумма += я;
+
+
+
+            // }
+
+
+
+            //}
+
+            //Console.WriteLine(сумма);
+
+
+
+            //5
+
+            //Console.WriteLine("Введите число:");
+
+            //int i=int.Parse(Console.ReadLine());
+
+            //интервал с = 0;
+
+            //пока (i>0)
+
+            //{
+
+            // я /= 10;
+
+            // с++;
+
+            //}
+
+            //Console.WriteLine(s);
         }
     }
 }
