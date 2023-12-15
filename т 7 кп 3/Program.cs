72ca8ce49c269e00d1b8d26b8d09a50767629d02
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2023-12-15 22:06:31 +0200

    Add files via upload

---
+++ т 7 кп 3/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace т_7_кп_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //строка st = "";
+
+            //int l = st.Length;
+
+            //строка s = "";
+
+            //for (int i = 0; i < 5; i++)
+
+            //{
+
+            // Console.Write($"Введите {i + 1}-е слово");
+
+            // строка st1 = Console.ReadLine();
+
+            // if(st1.Length>l)
+
+            // {
+
+            // l = st1.Длина;
+
+            // с = ст1;
+
+            // }
+
+            // Console.WriteLine($"Самое длинное слово {s} содержит {l} символов");
+
+            //}
+
+            //Задача 2
+
+            //строка st = Console.ReadLine();
+
+            //for (int i = 0; i < st.Length; i++)
+
+            //{
+
+            // если (st[i] == 'e')
+
+            // {
+
+            // st = st.Replace("e", "");
+
+            // }
+
+            // если (st[i] == 'E')
+
+            // {
+
+            // st = st.Replace("E", "");
+
+            // }
+
+            //}
+
+
+
+
+
+            //Консоль.Запись(ст);
+
+            //Задача 3
+
+            //строка st = Console.ReadLine();
+
+            //интервал с = 0;
+
+            //for (int i = 0; i < st.Length; i++)
+
+            //{
+
+            // if (st[i] == 'А' || st[i] == 'а')
+
+            // {
+
+            // с++;
+
+
+
+            // }
+
+
+
+            //}
+
+            //Console.WriteLine(s);
+
+            //задача 4
+
+            //строка st = Console.ReadLine();
+
+            //интервал я;
+
+            //for (i = 0; st[i] != ' '; i++)
+
+            //{
+
+
+
+            //}
+            //Задача 6
+            //строка s = Console.ReadLine();
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            // if (s[i] == 'а' || s[i] == 'А')
+            // {
+            // s = s.Replace("А", "О");
+            // s = s.Replace("а", "о");
+
+            // }
+            //}
+            //Console.WriteLine($"{s}");
+
+            //Задача 7
+            //Console.WriteLine("Введите слова:");
+            //строка s = Console.ReadLine();
+            //строка r= "";
+            //for (int i = s.Length - 1; i >= 0; i--)
+            //{
+            // г += s[i];
+            //}
+            //Console.WriteLine("Реверсированная строка:"+r);
+            Консоль.ReadKey();
+        }
+    }
+}
