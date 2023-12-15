83ddb5bc828fed81b62c8c4b28bdfeccf087a1d5
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2023-12-15 22:35:02 +0200

    Add files via upload

---
+++ т 8 кп 3/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace т_8_кп_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 4
+
+            //Стр();
+
+            //2 4
+
+            //3 4
+
+            //двойной x = Func(8, 5) + Func(6, 12) + Func(7,21);
+
+            //Console.WriteLine($"x={x:f1}");
+
+            //4 4
+
+            //Console.Write("x=");
+
+            //int x = int.Parse(Console.ReadLine());
+
+            //Console.Write("y=");
+
+            //int y = int.Parse(Console.ReadLine());
+
+            //int z = Max(x, 2 * y) + Max(2 * x - y, y);
+
+            //Console.WriteLine($"z={z}");
+
+            //Консоль.ReadKey();  
+            //2 4
+            //Трейг();
+
+        }
+
+        //1 4
+
+        //статическая пустота Str()
+
+        //{
+
+        // for (int i = 0; i < 5; i++)
+
+        // {
+
+        // Console.WriteLine("");
+
+        // for (int j = 0; j < 15; j++)
+
+        // {
+
+        // Console.Write("*");
+
+        // }
+
+        // }
+
+        //}
+
+        //3 4
+
+        //статический двойной Func(int x,int y)
+
+        //{
+
+        // возвращаем (Math.Sqrt(x)+y)/(Math.Sqrt(y)+x);
+
+        //}
+
+        //4 4
+
+        // статический int Max(int ​​x, int y)
+
+        //{
+
+        // если (х > у)
+
+        // {
+
+        // возвращаем х;
+
+        // }
+
+        //    еще
+
+        // {
+
+        // возвращаем у;
+
+        // }
+
+        //}
+        //2 4
+        //статическая пустота Treyg()
+        //{
+        // int k = 14;
+        // int u = 2;
+        // for (int s = 0; s < 25; s++)
+        // {
+        // если (s >= 4)
+        // {
+        // если (u <= 15)
+        // {
+        // у++;
+        // for (int j = 2; j <= u; j++)
+        // {
+        // Console.Write(" ");
+        // }
+        // }
+
+        // for (int i = 0; i < k; i++)
+        // {
+        // Console.Write("#");
+        // }
+        // к--;
+        // }
+        // Console.WriteLine("");
+
+    }
+}
+}
