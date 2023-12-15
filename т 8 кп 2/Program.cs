68b72a33dd2d0ab9314196c56f7f3d327df16cc9
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2023-12-15 22:18:07 +0200

    Add files via upload

---
+++ т 8 кп 2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace т_8_кп_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //символ();
+
+            //Задача 2
+            //Трейг();
+
+            //Задача 3
+            //двойной y = Func(5)+Func(12)+Func(19);
+            //Console.WriteLine($"x={y}");
+
+            //Задача 4
+            //Console.Write("Введите a=");
+            //int a=Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите b=");
+            //int b=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Число {Max(a,b)} большее");
+            Console.ReadKey();
+        }
+
+        //Задача 1
+        //публичный статический символ void()
+        //{
+        // for(int i=0;i<60;i++)
+        // {
+        // Console.Write("*");
+        // }
+        //}
+
+        //Задача 2
+        //публичная статическая пустота Treyg()
+        //{
+        // for (int i = 0; i < 8; i++)
+        // {
+        // for (int j = 0; j <i; j++)
+        // {
+        // Console.Write("#");
+        // }
+        // Console.WriteLine();
+        // }
+        //}
+
+        //Задача 3
+        //public static double Func(double x)
+        //{
+        // возвращаем (Math.Sqrt(x)+x)/2;
+        //}
+
+        //Задача 4
+        //public static int Max(int ​​a, int b)
+        //{
+        // если(а>b)
+        // {
+        // возвращаем а;
+        // }
+        //    еще
+        // {
+        // возвращаем б;
+        // }
+        //}
+    }
+}
+}
