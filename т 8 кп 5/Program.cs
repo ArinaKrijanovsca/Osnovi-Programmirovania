14cda4d744bb341545b3a75160446bca0eeeb145
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2023-12-17 16:14:34 +0200

    Add files via upload

---
+++ т 8 кп 5/Program.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace т_8_кп_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //статический двойной Func(int x)
+            //{
+            // double y = (x + Math.Sin(x))/3;
+            // возвращаем у;
+            //}
+            //2
+            //статические int Seconds(int h,int m, int s)
+            //{
+            //м += ч * 60;
+            //с += м * 60;
+            // возвращаем h*3600+m*60+s;
+            //}
+            //5 2
+            //статические недействительные числа (int n)
+            //{
+            // for (int i = 2; i <= n; i++)
+            // {
+            // если (я % 2 == 0)
+            // {
+            // Console.WriteLine(i);
+            // }
+            // }
+            //}
+            //5 3
+            //статический двойной Avarage(int a, int b)
+            //{
+            //интервал с = 0;
+            //for (int i = a; i <= b; i++)
+            //{
+            //с += я;
+            //}
+            //возврат с/б;
+            //}
+            //статическая пустота Main(string[] args)
+            //{
+            //1
+            //double y = (1 + Math.Sin(1) + 5 + Math.Sin(5) + 7 + Math.Sin(7))/3;
+            //двойной z = Func(1)+Func(5)+Func(7);
+            //Console.WriteLine(z);
+            //Console.WriteLine(y);
+            //2
+            //Console.Write("Введите h:");
+            //int h = int.Parse(Console.ReadLine());
+            //Console.Write("Введите m:");
+            //int m = int.Parse(Console.ReadLine());
+            //Console.Write("Введите s:");
+            //int s = int.Parse(Console.ReadLine());
+            //int z = Секунды(ч, м, с);
+            //Console.WriteLine($"z={z}");
+            //5 2
+            //Console.Write("Введите: ");
+            //int n = int.Parse(Console.ReadLine());
+            //Числа(n); //5 3 Console.Write("Введите:");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("Введите b:");
+            //int b = int.Parse(Console.ReadLine());
+            //double z=Avarage(a, b);
+            //Console.WriteLine(z);
+            //Console.ReadKey();
+        }
+    }
+}
