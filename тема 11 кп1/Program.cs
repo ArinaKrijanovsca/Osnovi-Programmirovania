03e828373947163dc9c46ff11945e121dc773f72
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2023-12-17 16:23:39 +0200

    Add files via upload

---
+++ тема 11 кп1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_11_кп1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Человек person = новый Person();
+            // person.name = "Иван";
+            // person.surname = "Лавриков";
+            // person.age = 30;
+            // //person.OutPut();
+            // Человек person1 = новый Person();
+            // person1.name = "Артем";
+            // person1.surname = "Сиглов";
+            // person1.age = 16;
+            //// person1.OutPut();
+            //Console.WriteLine(person1.ToString());
+            // Человек person2 = новый Person();
+            // person2.name = "илья";
+            // person2.surname = "абабий";
+            // person2.age = 16;
+
+            //Человек человек;
+            // человек = новый человек();
+            //Адрес адрес = новый адрес();
+            // адрес.индекс = 3300;
+            // адрес.страна = "Молдова";
+            // адрес.город = "Тирасполь";
+            // адрес.улица = "Карла Лип";
+            // адрес.дом = 5;
+            // адрес.апарт = 10;
+            // Console.WriteLine(address.ToString());
+            //Собака Neapolitan_Mastiff = новая собака();
+            //Мальтийская собака = новая собака();
+            //Dog Chow_Chow = новая собака();
+            //Console.ReadKey();
+
+        }
+    }
+}
