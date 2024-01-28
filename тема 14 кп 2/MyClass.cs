d074def4ef65211277244670ec35d22b6656ef18
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2024-01-28 15:04:46 +0200

    Add files via upload

---
+++ тема 14 кп 2/MyClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace тема_14_кп_2
+{
+    internal class MyClass
+    {
+        double value1, value2;
+        static int count;
+        public MyClass(double value1, double value2)
+        {
+            this.Value1 = value1;
+            this.Value2 = value2;
+            count++;
+        }
+        public MyClass()
+        {
+            this.Value1 = 2.0;
+            this.Value2 = 2.0;
+            count++;
+        }
+        static MyClass()
+        {
+            count = 0;
+        }
+        public double Value1
+        {
+            get
+            {
+                return value1;
+            }
+            set
+            {
+                value1 = value; 
+            }
+        }
+        public double Value2
+        {
+            get
+            {
+                return value2;
+            }
+            set
+            {
+                value2= value;
+            }
+        }
+        public override string ToString ()
+        {
+            return $" value1={Value1}\tvalue2={value2}";
+        }
+
+    }
+}
---
+++ тема 14 кп 2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_14_кп_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass  myclass1 = new MyClass();
+            MyClass myclass2 = new MyClass(20,30);
+            Console.WriteLine(myclass2.ToString());
+            Console.ReadKey();
+        }
+    }
+}
