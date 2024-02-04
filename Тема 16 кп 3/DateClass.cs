b0286c7ba6e3c977931fa3e3235016b70cc4afb6
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2024-02-04 19:31:57 +0200

    Add files via upload

---
+++ Тема 16 кп 3/DateClass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тема_16_кп_3
+{
+    internal class DateClass
+    {
+        /// <summary>
+        /// поле описывающее дату 
+        /// </summary>
+        DateTime data;
+        /// <summary>
+        /// конструктор без параметров 
+        /// </summary>
+        public DateClass() { }
+        /// <summary>
+        /// конструктор с 3 параметрами год месяц и день
+        /// </summary>
+        /// <param name="year">year</param>
+        /// <param name="month">month</param>
+        /// <param name="day">day</param>
+        public DateClass(int year,int month,int day) 
+        { 
+        this.Data = new DateTime (year,month,day);   
+        }
+        /// <summary>
+        /// свойство для поля дата 
+        /// </summary>
+        public DateTime Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        /// <summary>
+        /// метод позволяющий вычислить дату предыдущего дня
+        /// </summary>
+        /// <returns></returns>
+        public DateTime Yesterday()
+        { 
+            return data.AddDays(-1);
+        }
+        /// <summary>
+        /// метод позволяющий вычислить дату следующего дня 
+        /// </summary>
+        /// <returns></returns>
+        public DateTime Tomorrow() 
+        {
+            return data.AddDays(-1);
+        }
+        /// <summary>
+        /// метод  похволяющий вычислить скллько дней осталось до конца месяца 
+        /// </summary>
+        /// <returns></returns>
+        public int  EndMonth() 
+        {
+        return DateTime.DaysInMonth (data.Year, data.Month)-data.Day;
+        }
+        /// <summary>
+        ///  метод для вывода информации 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"year{data.Year}\nmonth{data.Month}\nday{data.Day}\n{Data.ToShortDateString()}\nYesterday{Yesterday()}\nTomorrow{Tomorrow()}\nEndMonth{EndMonth()}";
+        }
+
+    }
+}
---
+++ Тема 16 кп 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тема_16_кп_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// Здачча1
+            int year = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            int day = int.Parse(Console.ReadLine());
+            DateClass data1 = new DateClass(year,month,day);
+            Console.WriteLine($"{ data1.Yesterday()}\t{data1.Tomorrow()}\t{data1.EndMonth()}");
+            //Задача 2
+            DateClass data2 = new DateClass(year,month,day);
+            Console.WriteLine(data2.ToString());
+            Console.ReadKey();
+        }
+    }
+}
