2e9b3a77af5a456e5c511c425c38085e8da76a5b
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2024-02-18 19:46:49 +0200

    Add files via upload

---
+++ тема 17 кп 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_17_кп_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Staff> staffs = new List<Staff>();
+            staffs.Add(new Staff("Арина","Крижановская",new DateTime (2006,05,17),5000));
+            staffs.Add(new Meneger("Арина", "Крижановская", new DateTime(2006, 05, 17), 5000,12));
+            int k=int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            foreach(var staff in staffs) { staff.OutPut();staff.Doxod(k,n); }
+
+            Console.ReadKey();
+        }
+    }
+}
