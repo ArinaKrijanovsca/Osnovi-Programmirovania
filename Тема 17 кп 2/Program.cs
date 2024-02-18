4ab88666cfb6734250cfee17691ef86287cc3e2c
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2024-02-18 18:36:48 +0200

    Add files via upload

---
+++ Тема 17 кп 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тема_17_кп_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee("Крижановская ","Арина",5000,2));
+            employees.Add(new Engineer("Цвигуненко ", "Юлия", 4000, 5,4));
+            foreach (var st in employees)
+            { st.OutPut(); st.Doxod(); { Console.WriteLine(st.Doxod()); }; }
+           
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
