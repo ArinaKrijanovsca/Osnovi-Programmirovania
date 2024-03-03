3e9973a9ba6b3ddc00b8b8ef599a5db34e5b8241
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2024-03-03 15:23:35 +0200

    Add files via upload

---
+++ тема 18 кп 4/Enrollee.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_18_кп_4
+{/// <summary>
+/// наследуемый класс абитуриент
+/// </summary>
+    internal class Enrollee:Person
+    {
+        /// <summary>
+        /// поле поисвающее факультет
+        /// </summary>
+        string faculty;
+        /// <summary>
+        /// свойсво для поля факулитет
+        /// </summary>
+        public string Faculty { get => faculty; set => faculty = value; }
+        /// <summary>
+        /// конструкто по умолчанию
+        /// </summary>
+        public Enrollee() { }
+        /// <summary>
+        /// конструктор с параметрами 
+        /// </summary>
+        /// <param name="name">name</param>
+        /// <param name="surname">surname</param>
+        /// <param name="dateB">dateB</param>
+        /// <param name="faculty">faculry</param>
+        public Enrollee(string name, string surname, DateTime dateB,string faculty) :base(name,surname,dateB)
+        {
+        Faculty= faculty;
+        }
+        /// <summary>
+        /// метод Age для определения возраста
+        /// </summary>
+        /// <returns></returns>
+        public override int Age()
+        {
+            DateTime dateNow = DateTime.Now;
+            if (dateNow.Month < DateB.Month)
+            {
+                return dateNow.Year - DateB.Year - 1;
+            }
+            else if (dateNow.Month == DateB.Day)
+            {
+
+                if (dateNow.Day < DateB.Day)
+                {
+
+                    return dateNow.Year - DateB.Year - 1;
+
+                }
+                else
+                {
+                    return dateNow.Year - DateB.Year;
+                }
+            }
+            else
+            {
+                return dateNow.Year - DateB.Year;
+            }
+
+        }
+        /// <summary>
+        /// метод для вывода информации об объекте 
+        /// </summary>
+        /// <returns></returns>
+        public static Enrollee Init()
+        {
+            try
+            {
+                Console.Write("Имя:");
+                string name = Console.ReadLine();
+                Console.Write("Фамилия:");
+                string surname = Console.ReadLine();
+                Console.Write("Дата рождения:");
+                DateTime dateB = DateTime.Parse(Console.ReadLine());
+                Console.Write("Факультет:");
+                string facult = Console.ReadLine();
+                return new Enrollee(name, surname, dateB, facult);
+            }
+            catch { return Init(); }
+        }
+        /// <summary>
+        /// метод для вывода информации
+        /// </summary>
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Факультет:{faculty}");
+        }
+    }
+}
---
+++ тема 18 кп 4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_18_кп_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> person = new List<Person>();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Введите кого добавить:");
+                string status = Console.ReadLine();
+                switch (status.ToLower())
+                {
+                    case "e": Enrollee enrollee = new Enrollee(); person.Add(Enrollee.Init()); break;
+                    case "s": Student student = new Student(); person.Add(Student.Init()); break;
+                    case "t": Teacher teacher = new Teacher(); person.Add(Teacher.Init()); break;
+                    default: Console.Clear(); flag = false; break;
+                }
+            }
+            foreach (var elem in person)
+            {
+                elem.Print();
+            }
+            foreach (var elem in person)
+            {
+                if (elem.Age() > 18 && elem.Age() < 35)
+                {
+                     elem.Print();
+                }
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
