d6d64ae9ee13e1d9490bb57c2706dc519636d4fc
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2024-04-07 16:14:24 +0300

    Add files via upload

---
+++ тема 18 кп 3/Figure.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_18_кп_3
+{/// <summary>
+/// абстрактный класс
+/// </summary>
+    abstract class Figure
+    {
+
+        /// <summary>
+        /// поле для названия фигуры
+        /// </summary>
+        string name;
+        /// <summary>
+        /// конструктор по умолчанию
+        /// </summary>
+        public Figure() { }
+        /// <summary>
+        /// конструктор с параметрами
+        /// </summary>
+        /// <param name="name">название</param>
+        public Figure(string name)
+        { this.name = name; }
+        /// <summary>
+        /// свойство названия фигуры
+        /// </summary>
+        public string Name { get { return name; } set { name = value; } }
+        /// <summary>
+        /// абстрактный метод нахождения площади
+        /// </summary>
+        /// <returns>площадь фигуры</returns>
+        abstract public double Area();
+        /// <summary>
+        /// абстрактный метод нахождения периметра
+        /// </summary>
+        /// <returns>периметр</returns>
+        public abstract double Perimeter();
+        /// <summary>
+        /// метод для вывода информации об объекте
+        /// </summary>
+        /// <returns>поля класса</returns>
+        public override string ToString()
+        {
+            return name;
+        }
+
+    }
+}
