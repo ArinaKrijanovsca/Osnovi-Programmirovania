13ff7f7fa3f441340db30c12421ea3e89398b856
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2024-04-07 16:37:54 +0300

    Add files via upload

---
+++ тема 18 кп 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_18_кп_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Free free = new Free("Notepad++ Team", "Notepad++");
+            Shareware trial = new Shareware("Adobe", "Adobe Photoshop", new DateTime(2025, 02, 14), new DateTime(2024, 02, 12));
+            Commercial commercial = new Commercial("Microsoft", "Microsoft Office", new DateTime(2024, 02, 28), 200, new DateTime(2025, 07, 26));
+            List<Software> softWares = new List<Software>() { free, trial, commercial };
+            foreach (var item in softWares)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            foreach (var item in softWares)
+            {
+                Console.WriteLine(item.IsUsableOnDate());
+            }
+            Console.ReadLine();
+        }
+    }
+}
