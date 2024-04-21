1fd7d679dffcb0c96f21a877ab5ab295eaa43b28
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2024-04-21 12:04:36 +0300

    Add files via upload

---
+++ тема 19 кп 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_19_кп_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Touroperator> list = new List<Touroperator>();
+            list.Add(new Touroperator("Фирма1",2009,1,3000));
+            list.Add(new Touroperator("Фирма2",2008,2,2500));
+            list.Add(new Touroperator("Фирма3",2010,3,35000));
+            Console.WriteLine("Введите название фирмы:");
+            string name=Console.ReadLine();
+            bool not_find = false;
+            for (int i = 0;i<list.Count;i++)
+            {
+                if (list[i].Name==name)
+                {
+                    Console.WriteLine($"INN{list[i].Id} Price:{list[i].Profit}");
+                    not_find = true;
+                }
+            }
+            if(not_find==false)
+            {
+                Console.WriteLine("Фирма не найдена");
+            }
+
+           
+            Console.ReadKey();
+        }
+    }
+}
+
