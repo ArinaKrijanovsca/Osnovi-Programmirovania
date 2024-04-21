7a819a7a1b822ed5a87d1e592f8640a89f83187a
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2024-04-21 12:38:56 +0300

    Add files via upload

---
+++ тема 19 кп 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_19_кп_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> list = new List<Person>() { new Person("Аретм",22),new Person ("Илья",34),new Person("Леонид",24), new Person("Борис", 43) };
+            foreach (Person person in list)
+            {
+                Console.WriteLine(person.ToString());
+            }
+            list.Sort();
+            Console.WriteLine("\nSort complete\n");
+            foreach (Person person in list)
+            { 
+                Console.WriteLine(person.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ тема 19 кп 3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("тема 19 кп 3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("тема 19 кп 3")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ffa9ed09-b5f2-4615-bf36-f0c782650d57")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
