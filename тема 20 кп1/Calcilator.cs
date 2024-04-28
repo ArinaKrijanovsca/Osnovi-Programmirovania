bd99d2d84010f44b38fff0455455d348a6f54c85
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2024-04-28 23:02:42 +0300

    Add files via upload

---
+++ тема 20 кп1/Calcilator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_20_кп1
+{
+    internal class Calcilator<T>
+    {/// <summary>
+    /// поле описывающее у
+    /// </summary>
+        public dynamic x;
+        /// <summary>
+        /// поле описывающее х
+        /// </summary>
+        public dynamic y;
+        /// <summary>
+        /// метод реалезующий сложения
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <returns></returns>
+        public T Add(T x1, T y1)
+        {
+            dynamic x =  x1;
+            dynamic y = y1;
+            return x + y;
+        }
+        public T Sub(T x1, T y1)
+        {
+            dynamic x = x1;
+            dynamic y = y1;
+            return x -1;
+        }
+        /// <summary>
+        /// метод реалезующий  сложение
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <returns></returns>
+        public T Mul(T x1, T y1)
+        {
+            dynamic x = x1;
+            dynamic y = y1;
+            return x + y;
+        }
+
+        /// <summary>
+        /// иетод реалезующий деление 
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <returns></returns>
+        public T Div(T x1, T y1)
+        {
+            dynamic x = x1;
+            dynamic y = y1;
+            if (y!=0)
+            {
+                return x / y;
+            }
+            else
+            {
+                return y;
+            }
+        }
+
+    }
+}
---
+++ тема 20 кп1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_20_кп1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calcilator<int> c1= new Calcilator<int>();
+            Console.WriteLine(c1.Add(10,11));
+            Console.WriteLine(c1.Sub(12, 10));
+            Console.WriteLine(c1.Mul(13, 12));
+            Console.WriteLine(c1.Div(10, 2));
+            Calcilator<double> c2 = new Calcilator<double>();
+            Console.WriteLine(c2.Add(12.5, 4.2));
+            Console.WriteLine(c2.Sub(15.5, 5));
+            Console.WriteLine(c2.Mul(20.4, 12));
+            Console.WriteLine(c2.Div(16.5, 2));
+            Calcilator<string> c3 = new Calcilator<string>();
+            Console.WriteLine(c3.Add("14.8", "13"));
+            Console.WriteLine(c3.Sub("18", "10"));
+            Console.WriteLine(c3.Mul("9", "12"));
+            Console.WriteLine(c3.Div("13", "5.2"));
+            Console.ReadLine();
+
+        }
+    }
+}
