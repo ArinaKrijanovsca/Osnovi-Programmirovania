d68159836547c6beba7a63930cc03f27516b83b9
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2024-04-28 23:24:30 +0300

    Add files via upload

---
+++ тема 20 кп 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_20_кп_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle<int> t1 = new Triangle<int>(13, 15, 19);
+                t1.Print();
+            Console.WriteLine($"Площадь:{t1.S()}");
+            Triangle<uint> t2 = new Triangle<uint>(13, 15, 19);
+            t2.Print();
+            Console.WriteLine($"Площадь:{t2.S()}");
+            Triangle<double> t3 = new Triangle<double>(13, 15, 19);
+            t3.Print();
+            Console.WriteLine($"Площадь:{t3.S()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ тема 20 кп 2/Triangle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тема_20_кп_2
+{/// <summary>
+/// обощенный класс
+/// </summary>
+/// <typeparam name="T"></typeparam>
+    internal class Triangle<T>
+    {
+        /// <summary>
+        /// поле описывающее сторону а
+        /// </summary>
+        public T a;
+        /// <summary>
+        /// полек описывающее сторону б
+        /// </summary>
+        public T b;
+        /// <summary>
+        /// поле описывающее сторону с
+        /// </summary>
+        public T c;
+        /// <summary>
+        /// сввойтсво для поня а
+        /// </summary>
+        public T A { get; set; }
+        /// <summary>
+        /// свойство для поля б
+        /// </summary>
+        public T B { get; set; }
+        /// <summary>
+        /// свойтсво для поня с
+        /// </summary>
+        public T C { get; set; }
+        /// <summary>
+        /// конструткор с параметрами
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        public Triangle(T a, T b, T c   )
+        {
+            A = a;
+            B = b;
+            C = c;
+            
+        }
+        /// <summary>
+        /// метод для вывода информации
+        /// </summary>
+        public void Print   ( )
+        {
+            Console.WriteLine($"a={A},b={B},c={C}");
+        }
+        /// <summary>
+        ///  метод, возвращающий площадь треугольника
+        /// </summary>
+        /// <returns></returns>
+        public T S()
+        {
+            dynamic x = A;
+            dynamic y = B;
+            dynamic z= C; 
+            dynamic p=(x+y+z)/3;
+            return Math.Sqrt(p*(p-x)*(p-y)*(p-z));
+        }
+
+    }
+}
