0b889ca758d086430da071277984fb0391843670
ArinaKrijanovsca <147629263+ArinaKrijanovsca@users.noreply.github.com>
2024-06-02 23:01:05 +0300

    Add files via upload

---
+++ 31.05.24/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31._05._24
+{
+    internal class Program
+    {//28 и 29 задача
+        static void Main(string[] args)
+        {
+
+            Tour tour = new Tour();
+            tour.TourCalc();
+            tour.TourCalc("Италия");
+            tour.TourCalc("Армения", 2000);
+
+            //Tovar tovar = new Tovar("Черника", 13, 8);
+            //Tovar tovar1 = new Tovar("Мандарины", 18, 2);
+            //Console.WriteLine($"Стоимость всей чериники: {tovar.Sum()}");
+            //Console.WriteLine($"Стоимость всех мандарин: {tovar1.Sum()}");
+            //Console.WriteLine($"Стоимость всей черники и бананов: {tovar.Sum() + tovar1.Sum()}"); ;
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 31.05.24/Tour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31._05._24
+{
+    internal class Tour
+    {
+        string name;
+        double price;
+        int days;
+
+        public Tour() { }
+
+        public void TourCalc()
+        {
+            name = "Средиземное  море,бесплатно";
+            price = 0;
+            Console.WriteLine(name, price);
+        }
+        public void TourCalc(string name)
+        {
+            days = 1;
+            price = 100;
+            Console.WriteLine($"{days} день, {price} руб");
+        }
+        public void TourCalc(string name, int days)
+        {
+
+            price = 100 * days;
+            Console.WriteLine($"Цена: {price} руб");
+        }
+
+    }
+}
